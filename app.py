5ed222973931fd9ff32f1da76ca1b35156c6468c
umtty-hub <pilgodu@naver.com>
2026-02-22 18:26:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 st.set_page_config(page_title="마트 주문판", layout="wide")
 
-URL = "https://docs.google.com/spreadsheets/d/1a4UxIF3umGoGF4ZWc7BjwuMEiXj2NG5OFn7TO5H3lb4/edit?gid=0#gid=0"
+URL = ""
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
@@ -15,4 +15,4 @@ st.title("🛒 마트 초간편 주문")
 t1, t2 = st.tabs(["📝 주문하기", "🔍 주문확인"])
 
 except Exception as e:
-st.error(f"오류발생: {e}")
+st.error(f"⚠️ 연결 오류: {e}")
